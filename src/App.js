896d955cb80233e4a69c1dca6cac71b158af6651
Lukáš Jahoda <lukas.jahoda@softacus.com>
2022-06-27 15:10:15 +0200

    fix bug where new user gets blank screen

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React, {useContext, useState, useEffect, createElement} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import Greetings from "./Components/Greetings";
 import Quote from "./Components/Quote";
 import Time from "./Components/Time";
 import ToDoMini from "./Components/ToDoMini";
 import Weather from "./Components/Weather";
-import {FiMenu} from "react-icons/fi";
-import {User} from "./Contexts/UserContext";
+import { FiMenu } from "react-icons/fi";
+import { User } from "./Contexts/UserContext";
 import Hello from "./Components/Hello";
 import Todos from "./Components/Todos";
-import {FiSettings} from "react-icons/fi";
+import { FiSettings } from "react-icons/fi";
 import Settings from "./Components/Settings";
 import {FaDownload, FaPaintBrush} from "react-icons/fa";
 import Personalize from "./Components/Personalize";
 import fallBackImage from "./data/fallback_bg.jpg";
-import {MdOutlineTimer} from "react-icons/md";
+import { MdOutlineTimer } from "react-icons/md";
 import Pomodoro from "./Components/Pomodoro";
 
 const App = () => {
-    const {user} = useContext(User); //importing the user context which holds the values => name, verified status, theme and current location
+    const { user } = useContext(User); //importing the user context which holds the values => name, verified status, theme and current location
     const [todo, setTodo] = useState(false); //Stores the notes
     const [settings, setSettings] = useState(false); // stores toggle state of settings box
     const [pomodoroToggle, setpomodoroToggle] = useState(false);
     const [personalize, setPersonalize] = useState(false); //stores toggle state of personalize box
     //const [bg, setBg] = useState(fallBackImage)
 
+    //Function to fetch the theme from the localstorage, if found, assign it to bg_image, else assign "nature"
     const bg_image = () => {
         let storageImg = JSON.parse(localStorage.getItem("theme")).toString();
         if (!storageImg) {
@@ -52,7 +53,9 @@ const App = () => {
     }
 
     useEffect(() => {
-        bg_image();
+        if(user.name || user.currLocation || user.verified){
+            bg_image();
+        }
     });
 
     return (
@@ -60,9 +63,12 @@ const App = () => {
             {/* Check if the name, location, theme and verified exits in the localstorage aka, if the user is new or existing */}
             {/* {console.log(!user.name || !user.currLocation || !user.verified)} */}
             {!user.name || !user.currLocation || !user.verified ? (
-                <Hello/>
+                <Hello />
             ) : (
-                <div id="main-container" className="main__container h-screen px-5 py-2] bg-opacity-75 bg-cover bg-black">
+                <div id="main-container"
+                    className="main__container h-screen px-5 py-2] bg-opacity-75 bg-cover bg-black"
+
+                >
                     <div className="upper_container h-[10%] flex justify-between items-center">
                         {/* Top container having the todo menu and weather app */}
                         <div className="left text-white">
@@ -75,18 +81,18 @@ const App = () => {
                                     } else setTodo(true);
                                 }}
                             >
-                                <FiMenu/> {/* The menu icon imported from react-icon */}
+                                <FiMenu /> {/* The menu icon imported from react-icon */}
                             </button>
-                            {todo && <Todos/>}{" "}
+                            {todo && <Todos />}{" "}
                             {/* If the toggle state is true, show the todo component */}
                         </div>
                         <div className="right">
-                            <Weather/> {/* Weather component */}
+                            <Weather /> {/* Weather component */}
                         </div>
                     </div>
                     <div className="middle_container  h-[80%] flex flex-col justify-center items-center">
                         <div className="greetings mb-5">
-                            <Greetings/> {/* Greeting container */}
+                            <Greetings /> {/* Greeting container */}
                         </div>
                         <div className="time_conatiner bg-black bg-opacity-50 rounded-3xl px-7 ">
                             <div
@@ -94,7 +100,7 @@ const App = () => {
                                 onClick={() => setpomodoroToggle(true)}
                             >
                                 <p className="text-white font-base mr-1">Pomodoro</p>
-                                <MdOutlineTimer style={{color: "white"}} size={20}/>
+                                <MdOutlineTimer style={{ color: "white" }} size={20} />
                             </div>
                             {pomodoroToggle && (
                                 <Pomodoro
@@ -102,26 +108,25 @@ const App = () => {
                                     setpomodoroToggle={setpomodoroToggle}
                                 />
                             )}
-                            <Time/> {/* Time container */}
+                            <Time /> {/* Time container */}
                         </div>
-                        <div
-                            className="todo_main_container bg-black py-1 pb-3 rounded-lg px-10 bg-opacity-60 mt-4 shadow-2xl">
+                        <div className="todo_main_container bg-black py-1 pb-3 rounded-lg px-10 bg-opacity-60 mt-4 shadow-2xl">
                             <div className="main_tasks text-white text-2xl mt-3">
                                 What are the task for the day?
                             </div>
                             <div className="mini_todo_container">
-                                <ToDoMini openTodos={() => setTodo(true)}/>{" "}
+                                <ToDoMini openTodos={() => setTodo(true)} />{" "}
                                 {/* Todo input container */}
                             </div>
                         </div>
                         <div className="main_tasks ">
-                            <Quote/> {/* Quote component */}
+                            <Quote /> {/* Quote component */}
                         </div>
                     </div>
                     <div className="lower_container  h-[10%] flex justify-between items-center">
                         <div className="setting_div w-[28%] flex flex-col-reverse relative">
                             <FiSettings
-                                style={{color: "white"}}
+                                style={{ color: "white" }}
                                 onClick={() => {
                                     if (settings) {
                                         setSettings(false);
@@ -130,7 +135,7 @@ const App = () => {
                                     }
                                 }}
                             />
-                            {settings && <Settings/>}{" "}
+                            {settings && <Settings />}{" "}
                             {/* Showing the setting component if the toggle state is true */}
                         </div>
                         <div className="personalize text-white w-[28vw] flex justify-end relative">
@@ -144,7 +149,7 @@ const App = () => {
                                 }}
                             >
                                 Personalize
-                                <FaPaintBrush className="mx-2"/>
+                                <FaPaintBrush className="mx-2" />
                             </button>
                             {/* Showing the personalize menu when the toggle state is set to true */}
                             {personalize && (
@@ -154,10 +159,10 @@ const App = () => {
                                 />
                             )}
                             <button id="download-btn"
-                               className="flex items-center border-2 border-white rounded-full px-3 py-1 bg-black bg-opacity-40 hover:bg-white hover:bg-opacity-40 mx-2"
-                               onClick={() => {
-                                   download_img().then(r => r);
-                               }}
+                                    className="flex items-center border-2 border-white rounded-full px-3 py-1 bg-black bg-opacity-40 hover:bg-white hover:bg-opacity-40 mx-2"
+                                    onClick={() => {
+                                        download_img().then(r => r);
+                                    }}
                             >Download
                                 <FaDownload className="mx-2"/>
                             </button>
